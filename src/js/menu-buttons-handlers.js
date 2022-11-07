ae861a737f28f45186531c303eef5a4c9086d288
Xardas <vadikradik@yandex.ru>
2022-11-07 19:59:56 +0300

    modal call form added

--- src/js/menu-buttons-handlers.js
+++ src/js/menu-buttons-handlers.js
@@ -4,8 +4,8 @@ export let createHandlers = function() {
         let sideMenu = document.querySelector(".side-menu");
         sideMenu.classList.add("side-menu--open");
 
-        let fog = document.querySelector(".fog");
-        fog.classList.add("fog--visible");
+        let fog = document.querySelector(".fog-menu");
+        fog.classList.add("fog-menu--visible");
     });
 
     let sideMenuCloseButton = document.querySelector(".side-menu__close");
@@ -13,16 +13,42 @@ export let createHandlers = function() {
         let sideMenu = document.querySelector(".side-menu");
         sideMenu.classList.remove("side-menu--open");
 
-        let fog = document.querySelector(".fog");
-        fog.classList.remove("fog--visible");
+        let fog = document.querySelector(".fog-menu");
+        fog.classList.remove("fog-menu--visible");
     });
 
     window.addEventListener("resize", function(evt) {
         let openMenu = document.querySelector(".side-menu--open");
         if (openMenu) {
             openMenu.classList.remove("side-menu--open");
-            let fog = document.querySelector(".fog");
-            fog.classList.remove("fog--visible");
+            let fog = document.querySelector(".fog-menu");
+            fog.classList.remove("fog-menu--visible");
         }
+    });
+
+    let callButtons = document.querySelectorAll(".communications__call");
+    for (let i = 0; i < callButtons.length; i++) {
+        callButtons[i].addEventListener("click", function() {
+            let modalContainer = document.querySelector(".modal-container");
+            modalContainer.classList.add("modal-container--visible");
+
+            let modalCall = document.querySelector(".modal-call");
+            modalCall.classList.add("modal-call--open");
+
+            let fog = document.querySelector(".fog-modal");
+            fog.classList.add("fog-modal--visible");
+        });
+    }
+
+    let modalCallCloseButton = document.querySelector(".modal-call__button-close");
+    modalCallCloseButton.addEventListener("click", function() {
+        let modalContainer = document.querySelector(".modal-container");
+        modalContainer.classList.remove("modal-container--visible");
+
+        let modalCall = document.querySelector(".modal-call");
+        modalCall.classList.remove("modal-call--open");
+
+        let fog = document.querySelector(".fog-modal");
+        fog.classList.remove("fog-modal--visible");
     });
 }
