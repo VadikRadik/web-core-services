ca693fe79a66872425599919efb9b4ec7a78e1a0
Xardas <vadikradik@yandex.ru>
2022-11-08 18:20:08 +0300

    prices section added

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,12 @@ import '../scss/style.scss';
 
 import {initializeBrandsSection} from './brands.js';
 import {initializeKindsSection} from './kinds.js';
+import {initializePricesSection} from './prices.js';
 import {createHandlers} from './menu-buttons-handlers.js';
 
 initializeBrandsSection();
 initializeKindsSection();
+initializePricesSection();
 createHandlers();
 
 
